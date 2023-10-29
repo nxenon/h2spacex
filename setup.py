e0e3c2c740f6dda5c464555b54b30fc82adac5dc
M Amin Nasiri <nasiri.aminm@gmail.com>
2023-10-29 16:12:15 +0400

    update version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as readme_file:
 
 setup(
     name='h2spacex',
-    version='0.1.1',
+    version='0.1.2',
     description='HTTP/2 Single Packet Attack low level library based on Scapy',
     package_dir={"": "h2spacex"},
     packages=find_packages(where="h2spacex"),
