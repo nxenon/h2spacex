8786d99c2891ddd9426b6514612eaece07ac912e
M Amin Nasiri <nasiri.aminm@gmail.com>
2023-11-02 22:03:26 +0400

    add FrameParser class and decompression

--- h2spacex/h2_frames.py
+++ h2spacex/h2_frames.py
@@ -1,5 +1,157 @@
 import scapy.contrib.http2 as h2
 from scapy.packet import NoPayload
+import gzip
+import brotli
+import zlib
+
+
+def decompress_gzip_data(gzip_data: bytes):
+    # decompress gzip
+    try:
+        decompressed_content = gzip.decompress(gzip_data)
+        decoded_content = decompressed_content.decode('utf-8')
+    except Exception as e:
+        print('# Error in decompressing gzip encoded body : ' + str(e))
+        return gzip_data
+
+    return decoded_content
+
+
+def decompress_br_data(br_data: bytes):
+    # Decompress br
+    try:
+        decompressed_content = brotli.decompress(br_data)
+        decoded_content = decompressed_content.decode('utf-8')
+    except Exception as e:
+        print('# Error in decompressing br encoded body : ' + str(e))
+        return br_data
+
+    return decoded_content
+
+
+def decompress_deflate_data(deflate_data: bytes):
+    # Decompress the Deflate-compressed data
+    try:
+        decompressed_content = zlib.decompress(deflate_data, -zlib.MAX_WBITS)
+        decoded_content = decompressed_content.decode('utf-8')
+    except Exception as e:
+        print('# Error in decompressing deflate encoded body : ' + str(e))
+        return deflate_data
+
+    return decoded_content
+
+
+class FrameParser:
+    """
+    class for parsing response frames
+    it also handles some tasks: for example if server sends settings frame, it send ACK for it
+    """
+    def __init__(self, h2_connection=None, is_gzipped=False):
+        # headers and data frames list
+        # format --> {1: {'header': 'response header frame', 'data': b'response data frames'}}
+        # format --> {STREAM_ID: {'header': 'response header frame', 'data': b'response data frames'}}
+        self.headers_and_data_frames = {}
+        # H2 Connection Object (H2Connection OR H2OnTlsConnection)
+        self.h2_connection = h2_connection
+        self.headers_table = h2.HPackHdrTable()
+
+    def show_response_of_sent_of_sent_requests(self):
+        for s_id in self.headers_and_data_frames.keys():
+            headers = self.headers_and_data_frames[s_id]['header']
+            print(f'#-    Stream ID: {s_id}   -#')
+            print('-Headers-')
+            print(headers)
+            print('-Body-')
+            data = self.headers_and_data_frames[s_id]['data']
+            if 'content-encoding: gzip' in headers:
+                data = decompress_gzip_data(data)
+            elif 'content-encoding: br' in headers:
+                data = decompress_br_data(data)
+            elif 'content-encoding: deflate' in headers:
+                data = decompress_deflate_data(data)
+
+            print(str(data))
+
+    def add_frames(self, frames_bytes: bytes, is_verbose=False):
+        if frames_bytes:
+            parsed_frames = h2.H2Seq(frames_bytes).frames
+
+            for f in parsed_frames:
+                if is_verbose:
+                    print(f.show())
+
+                if isinstance(f.payload, h2.H2HeadersFrame):
+                    self.parse_header_frame(f)
+
+                elif isinstance(f.payload, h2.H2DataFrame):
+                    self.parse_data_frame(f)
+
+                elif isinstance(f.payload, h2.H2SettingsFrame):
+                    self.parse_settings_frame(f)
+
+                elif isinstance(f.payload, h2.H2WindowUpdateFrame):
+                    self.parse_window_update_frame(f)
+
+                elif isinstance(f.payload, h2.H2PingFrame):
+                    self.parse_ping_frame(f)
+
+                elif isinstance(f.payload, NoPayload):
+                    if f.type == 4:  # settings frame
+                        self.parse_settings_frame(f)
+
+                elif isinstance(f.payload, h2.H2ResetFrame):
+                    self.parse_reset_frame(f)
+
+                else:
+                    print('--frame--')
+                    print('Frame Type: ' + str(type(f.payload)) + ' / Type ID: ' + str(f.type))
+                    f.show()
+                    print('##frame##')
+
+
+    def parse_settings_frame(self, settings_frame):
+        if 'A' in settings_frame.flags:
+            print('* Server sent ACK for client SETTINGS frame')
+
+        else:
+            ack_settings_frame = create_settings_frame(is_ack=1)
+            self.send_frame(ack_settings_frame)
+            print('+ Client sent ACK for server SETTINGS frame')
+
+    def parse_window_update_frame(self, windows_update_frame):
+        print('* Server sent WINDOW UPDATE frame with win_increase_size of: ' + str(windows_update_frame.win_size_incr))
+
+    def parse_ping_frame(self, ping_frame):
+        if 'A' in ping_frame.flags:
+            print('* Server sent ACK for PING frame')
+
+    def parse_reset_frame(self, reset_frame):
+        print(f'# Server sent RESET frame for Stream ID: {reset_frame.stream_id}, with Err_Code: {reset_frame.error}')
+
+    def parse_header_frame(self, header_frame):
+        headers_string = self.get_headers_string_from_headers_frame(header_frame)
+        stream_id = header_frame.stream_id
+        if stream_id not in self.headers_and_data_frames:
+            self.headers_and_data_frames[stream_id] = {'header': headers_string, 'data': b''}
+        else:
+            self.headers_and_data_frames[stream_id]['headers'] += headers_string
+
+    def get_headers_string_from_headers_frame(self, headers_frame):
+        headers_string = self.headers_table.gen_txt_repr(headers_frame.hdrs)
+        return headers_string
+
+    def parse_data_frame(self, data_frame):
+        stream_id = data_frame.stream_id
+        if stream_id not in self.headers_and_data_frames:
+            self.headers_and_data_frames[stream_id] = {'header': '', 'data': data_frame.data}
+        else:
+            self.headers_and_data_frames[stream_id]['data'] += data_frame.data
+
+    def send_frame(self, frame):
+        if frame:
+            frame = bytes(frame)
+            if not self.h2_connection.is_connection_closed:
+                self.h2_connection.send_bytes(frame)
 
 
 def create_headers_frame(
@@ -77,14 +229,15 @@ def create_settings_frame(
     )
 
 
-def get_headers_string_from_headers_frame(headers_frame):
+_old_headers_table = h2.HPackHdrTable()
+def _get_headers_string_from_headers_frame(headers_frame):
     """
+    this function will be deprecated
     get string of response headers from headers frame
     :param headers_frame:
     :return:
     """
-    headers_table = h2.HPackHdrTable()
-    headers_string = headers_table.gen_txt_repr(headers_frame.hdrs)
+    headers_string = _old_headers_table.gen_txt_repr(headers_frame.hdrs)
     return headers_string
 
 
@@ -112,13 +265,22 @@ def parse_response_frames_bytes(
                 print(f.show())
 
             if isinstance(f.payload, h2.H2HeadersFrame):
-                headers_string = get_headers_string_from_headers_frame(f)
+                headers_string = _get_headers_string_from_headers_frame(f)
                 print(f'------ Headers Stream ID: {f.stream_id} ------')
                 print(headers_string)
 
             elif isinstance(f.payload, h2.H2DataFrame):
                 print(f'------ Data Stream ID: {f.stream_id} ------')
-                print(str(f.data))
+                with gzip.GzipFile(fileobj=gzip.io.BytesIO(f.data), mode='rb') as decompressed_file:
+                    # Read the decompressed data
+                    decompressed_content = decompressed_file.read()
+
+                # If the decompressed content is in bytes, you might want to decode it (if it contains text)
+                decoded_content = decompressed_content.decode('utf-8')
+                print(decoded_content)
+
+                # print(f'------ Data Stream ID: {f.stream_id} ------')
+                # print(str(f.data))
 
             elif isinstance(f.payload, h2.H2SettingsFrame):
                 if socket_obj is not None:
